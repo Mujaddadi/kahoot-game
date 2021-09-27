cfcfed93d5ad71c83fe538e19b763e5cbe0c65e3
Taha Hassan Mujaddadi <tahahassan82@gmail.com>
2021-09-27 20:58:08 +0300

    SingleResult component

---
+++ src/Game/components/Result/SingleResult.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SingleResult = () => {
+  return (
+    <div className="d-flex justify-content-between">
+      <span>B</span>
+      <span>2</span>
+      <span>90</span>
+    </div>
+  );
+};
+
+export default SingleResult;

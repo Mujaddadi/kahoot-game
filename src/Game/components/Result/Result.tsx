ca75e7edea0e36d5fca7b811134b602c83b7f374
Taha Hassan Mujaddadi <tahahassan82@gmail.com>
2021-09-27 22:52:11 +0300

    Linked new components to Result. Result is now showing actual data

--- src/Game/components/Result/Result.tsx
+++ src/Game/components/Result/Result.tsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import Button from "react-bootstrap/Button";
 
 import SingleResult from "./SingleResult";
+import ResultHeading from "./ResultHeading";
+
+import { getTotal } from "../../utilityFunctions";
+
+import GameContext from "../../gameContext";
+
+import "./Result.scss";
+
 const Result = () => {
+  const { selectedBoxes, ResetGame } = useContext(GameContext);
+
   return (
-    <div>
-      <h2>Player Items</h2>
-      <div className="d-flex justify-content-between">
-        <span>Item</span>
-        <span>QTY</span>
-        <span>Score</span>
+    <div className="mt-4">
+      <h2 className="text-center mb-4">Selected Items</h2>
+      <ResultHeading />
+      <div className="results">
+        {selectedBoxes.map((boxResult) => (
+          <SingleResult result={boxResult} />
+        ))}
       </div>
-      <SingleResult />
+
+      <div>Bonuses 30</div>
+      <div className="d-flex justify-content-between mt-4">
+        <div>
+          <div>Total </div>
+          <div>{getTotal(selectedBoxes)}</div>
+        </div>
+        <div>
+          <Button variant="success" onClick={ResetGame}>
+            New Game
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };

e7698015ff428a22c0ee776dbb81dd6153bca2eb
Taha Hassan Mujaddadi <tahahassan82@gmail.com>
2021-09-27 23:35:26 +0300

    Updated boxes data

--- src/Game/components/BoxesList/BoxesList.tsx
+++ src/Game/components/BoxesList/BoxesList.tsx
@@ -7,11 +7,11 @@ import "./BoxesList.scss";
 const BoxesList = () => {
   return (
     <div className="d-flex boxList">
-      <Box data={{ id: 1, points: 50, letter: "A" }} />
-      <Box data={{ id: 1, points: 30, letter: "B" }} />
-      <Box data={{ id: 1, points: 20, letter: "C" }} />
-      <Box data={{ id: 1, points: 15, letter: "D" }} />
-      <Box data={{ id: 1, points: 10, letter: "E" }} />
+      <Box data={{ id: 1, points: 50, letter: "A", quantity: 1 }} />
+      <Box data={{ id: 2, points: 30, letter: "B", quantity: 1 }} />
+      <Box data={{ id: 3, points: 20, letter: "C", quantity: 1 }} />
+      <Box data={{ id: 4, points: 15, letter: "D", quantity: 1 }} />
+      <Box data={{ id: 5, points: 10, letter: "E", quantity: 1 }} />
     </div>
   );
 };

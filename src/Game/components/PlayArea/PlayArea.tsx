fe37ccb7354f6f6b064ff3541597d1a584f72d3b
Taha Hassan Mujaddadi <tahahassan82@gmail.com>
2021-09-27 20:55:09 +0300

    PlayArea component

---
+++ src/Game/components/PlayArea/PlayArea.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import BoxesList from "../BoxesList";
+
+import "./PlayArea.scss";
+
+const PlayArea = () => {
+  return (
+    <div className="playArea">
+      <BoxesList />
+    </div>
+  );
+};
+
+export default PlayArea;

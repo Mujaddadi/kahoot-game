1eaa8671e2ce667c45fcff8004964fdbffc6e58b
Taha Hassan Mujaddadi <tahahassan82@gmail.com>
2021-09-27 22:48:03 +0300

    State to keep the selected box data and passed it through context

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,17 +7,33 @@ import Header from "./components/Header";
 import PlayArea from "./components/PlayArea";
 import Result from "./components/Result";
 
+import GameContext from "./gameContext";
+
+import { BoxData } from "./types";
+
 const Game = () => {
+  const [collection, setCollection] = useState<BoxData[]>([]);
+
+  const AddToCollection = (newBoxData: BoxData) => {
+    setCollection((collection) => [...collection, newBoxData]);
+  };
+
+  const ResetGame = () => setCollection([]);
+
   return (
-    <Row>
-      <Col sm={12} md={8}>
-        <Header />
-        <PlayArea />
-      </Col>
-      <Col sm={12} md={4}>
-        <Result />
-      </Col>
-    </Row>
+    <GameContext.Provider
+      value={{ AddToCollection, selectedBoxes: collection, ResetGame }}
+    >
+      <Row>
+        <Col sm={12} md={8}>
+          <Header />
+          <PlayArea />
+        </Col>
+        <Col sm={12} md={4}>
+          <Result />
+        </Col>
+      </Row>
+    </GameContext.Provider>
   );
 };
 

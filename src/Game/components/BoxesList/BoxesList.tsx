1de4be8295e269bedc8381cea2600321d279965a
Taha Hassan Mujaddadi <tahahassan82@gmail.com>
2021-09-28 22:18:26 +0300

    Render boxList from data

--- src/Game/components/BoxesList/BoxesList.tsx
+++ src/Game/components/BoxesList/BoxesList.tsx
@@ -4,14 +4,14 @@ import Box from "./Box";
 
 import "./BoxesList.scss";
 
+import boxData from "../../data";
+
 const BoxesList = () => {
   return (
     <div className="d-flex boxList">
-      <Box data={{ id: 1, points: 50, letter: "A", quantity: 1 }} />
-      <Box data={{ id: 2, points: 30, letter: "B", quantity: 1 }} />
-      <Box data={{ id: 3, points: 20, letter: "C", quantity: 1 }} />
-      <Box data={{ id: 4, points: 15, letter: "D", quantity: 1 }} />
-      <Box data={{ id: 5, points: 10, letter: "E", quantity: 1 }} />
+      {boxData.map((data) => (
+        <Box data={data} />
+      ))}
     </div>
   );
 };

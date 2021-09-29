e1f9516078be2aa32be64a0c5db89f1a24c41f7f
Taha Hassan Mujaddadi <tahahassan82@gmail.com>
2021-09-29 22:48:01 +0300

    Added some comments in utility files

--- src/Game/utilityFunctions.ts
+++ src/Game/utilityFunctions.ts
@@ -2,36 +2,41 @@ import cloneDeep from "lodash/cloneDeep";
 
 import { BoxData } from "./types";
 
+/**
+ * This is to add new boxes to collection
+ */
+
 export const addToCollection = (
   selectedBoxes: BoxData[],
   selectedBox: BoxData
 ) => {
-  let selectedBoxesCopy = cloneDeep(selectedBoxes);
+  let selectedBoxesClone = cloneDeep(selectedBoxes);
 
-  if (!selectedBoxesCopy.length) {
-    selectedBoxesCopy.push(selectedBox);
-    return selectedBoxesCopy;
+  //If the selectedBoxesClone is empty, just push the box and return
+  if (!selectedBoxesClone.length) {
+    selectedBoxesClone.push(selectedBox);
+    return selectedBoxesClone;
   }
 
   for (let index = 0; index < selectedBoxes.length; index += 1) {
+    //If the box already exist in the list, increase its quantity
     if (selectedBoxes[index].id === selectedBox.id) {
-      selectedBoxesCopy[index].quantity += 1;
-      return selectedBoxesCopy;
+      selectedBoxesClone[index].quantity += 1;
+      return selectedBoxesClone;
     }
   }
 
-  selectedBoxesCopy.push(selectedBox);
+  selectedBoxesClone.push(selectedBox);
 
-  return selectedBoxesCopy;
+  return selectedBoxesClone;
 };
 
-export const getTotal = (selectedBoxes: BoxData[]) => {
-  let boxesTotal = 0;
-  selectedBoxes.forEach((box) => (boxesTotal += getBoxTotal(box)));
-  return boxesTotal;
-};
-
+/**
+ * This is to get the individual box total including bonus
+ */
 export const getBoxTotal = (boxData: BoxData) => {
+  // If the remainder of box quantity and the bonus multiple is zero or there can be multiple bonus factors,
+  // add the bonus to totalMultiples
   if (
     boxData.bonus &&
     (boxData.quantity % boxData.bonus.multiple === 0 ||
@@ -41,17 +46,31 @@ export const getBoxTotal = (boxData: BoxData) => {
       boxData.quantity / boxData.bonus.multiple
     );
 
+    // Add total with the total bonuses
     return boxData.quantity * boxData.points + totalMultiples * boxData.points;
   }
 
   return boxData.quantity * boxData.points;
 };
 
+/**
+ * This is to get the total including bonuses
+ */
+export const getTotal = (selectedBoxes: BoxData[]) => {
+  let boxesTotal = 0;
+  selectedBoxes.forEach((box) => (boxesTotal += getBoxTotal(box)));
+  return boxesTotal;
+};
+
+/**
+ * This is to get the total bonus
+ */
 export const getBonusTotal = (selectedBoxes: BoxData[]) => {
   let totalBonus = 0;
 
   for (let index = 0; index < selectedBoxes.length; index += 1) {
     const currentBoxData = selectedBoxes[index];
+
     if (currentBoxData.bonus) {
       if (
         currentBoxData.quantity % currentBoxData.bonus.multiple === 0 ||
